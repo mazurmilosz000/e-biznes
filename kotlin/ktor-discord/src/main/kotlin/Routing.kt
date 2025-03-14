84db62d374909187fe41df40a1aceaefc5f05998
Miłosz Mazur <milosz.mazur@kambu.pl>
2025-03-14 12:23:56 +0100

    init kotlin project

---
+++ kotlin/ktor-discord/src/main/kotlin/Routing.kt
@@ -0,0 +1,17 @@
+package com.milosz000
+
+import io.ktor.server.application.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.slf4j.event.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
